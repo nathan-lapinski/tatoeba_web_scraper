d04add4df225d26b231e326063b2acb3fe03d57e
Nate L <n4t311@gmail.com>
2017-05-30 08:28:01 +0900

    restructuring directories

--- build/server.js
+++ build/server.js
@@ -31,4 +31,3 @@ app.get('/scrape/:kanji', function (req, res) {
 app.listen('8081');
 console.log('Magic happens on port 8081');
 exports = module.exports = app;
-//# sourceMappingURL=server.js.map
