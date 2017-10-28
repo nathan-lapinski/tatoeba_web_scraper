82cac43ca03404bfe2714b0cfd09c5615762cc40
Nathan Lapinski <nlapinski@spotx.tv>
2017-10-28 09:22:06 +0900

    enable cors by default for testing

--- app/server.ts
+++ app/server.ts
@@ -3,7 +3,13 @@ import * as express from 'express';
 import { RequestController } from './controllers';
 
 const app: express.Application = express();
-const port: number = process.env.PORT || 3000;
+const port: number = process.env.PORT || 9999;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use('/scrape', RequestController);
 
